a400d2218a12136b18fc0b8006bf7b446ce78b65
sitahlatif <sitah27@hotmail.com>
2019-02-10 13:32:26 +0300

    Sitah_Sultan

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def First_view(request):
+	context ={
+	'msg' : "Hello World!"
+	}
+	return render(request, "test.html", context)
--- task_02/settings.py
+++ task_02/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurants'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'task_02.urls'
